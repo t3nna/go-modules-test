161c9a9aa3d229ad85c84e65be0446ca43c98339
Ivan Melnyk <ivmelnyk@griddynamics.com>
2025-05-30 12:44:54 +0200

    third party package added

--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package golang_test_modules
 
+import "github.com/GoesToEleven/dog"
+
 func Bark() string {
 	return "Bark"
 }
 
 func Barks() string {
 	return Bark() + " " + Bark()
+}
+
+func MessageFromThirdPackage(s string) string {
+	return dog.WhenGrownUp(s)
 }
